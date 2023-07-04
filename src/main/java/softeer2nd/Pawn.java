d77b340628070de96bb2b0decbc28b15b630e37f
socra167 <socra167@gmail.com>
2023-07-04 10:24:15 +0900

    feat: 흰색이 부여되는 Pawn 기본 생성자 구현

--- src/main/java/softeer2nd/Pawn.java
+++ src/main/java/softeer2nd/Pawn.java
@@ -2,10 +2,13 @@ package softeer2nd;
 
 public class Pawn {
     String color;
+
     Pawn(String color) {
         this.color = color;
     }
-
+    Pawn() {
+        this.color = "white";
+    }
     String getColor() {
         return color;
     }
