67a409b0be573f93978a33398976060733a418e6
socra167 <socra167@gmail.com>
2023-07-05 12:52:24 +0900

    feat: 체스판의 col, row 사이즈를 받아 초기화하는 메소드 구현

--- src/main/java/softeer2nd/chess/Board.java
+++ src/main/java/softeer2nd/chess/Board.java
@@ -5,35 +5,47 @@ import softeer2nd.chess.pieces.Pawn;
 import java.util.*;
 
 public class Board {
+    private int rowSize;
+    private int colSize;
     private static final int ROW_SIZE = 8;
     private static final int COL_SIZE = 8;
     Map<String, Pawn> pawnMap = new HashMap<>();
 
     Board() {
-
+        this(8, 8);
     }
-    public void initialize() throws Exception {
+
+    Board(int row, int col) {
+        initialize(row, col);
+    }
+    public void initialize(int row, int col) {
         Pawn pawn;
-        for (int i = 0; i < ROW_SIZE; i++) {
-            if (i == 1) {
-                for (int j = 0; j < COL_SIZE; j++) {
-                    pawn = new Pawn(Color.BLACK);
-                    add(pawn, j, i);
-                }
-            } else if (ROW_SIZE - i == 2) {
-                for (int j = 0; j < COL_SIZE; j++) {
-                    pawn = new Pawn(Color.WHITE);
-                    add(pawn, j, i);
+        rowSize = row;
+        colSize = col;
+        pawnMap.clear();
+        try {
+            for (int i = 0; i < rowSize; i++) {
+                if (i == 1) {
+                    for (int j = 0; j < colSize; j++) {
+                        pawn = new Pawn(Color.BLACK);
+                        add(pawn, j, i);
+                    }
+                } else if (rowSize - i == 2) {
+                    for (int j = 0; j < colSize; j++) {
+                        pawn = new Pawn(Color.WHITE);
+                        add(pawn, j, i);
+                    }
                 }
             }
+        } catch (Exception e) {
         }
     }
     public String findEmpty() throws Exception { // 체스판의 비어 있는 공간 중 맨 앞의 키 반환
         Set<String> keys = pawnMap.keySet();
         StringBuilder sb = new StringBuilder();
         String location;
-        for (int i = 0; i < ROW_SIZE; i++) {
-            for (int j = 1; j < COL_SIZE + 1; j++) {
+        for (int i = 0; i < rowSize; i++) {
+            for (int j = 1; j < colSize + 1; j++) {
                 sb.append((char)('A' + 0));
                 sb.append(j);
                 if (!keys.contains(sb.toString())) {
@@ -47,23 +59,32 @@ public class Board {
     private String indexToLocation(int index) { // index 값을 체스판 맵의 키로 변환
         StringBuilder sb = new StringBuilder();
         String location;
-        sb.append((char)('A' + (index / COL_SIZE)));
-        sb.append(index % COL_SIZE + 1);
+        sb.append((char)('A' + (index / colSize)));
+        sb.append(index % rowSize + 1);
         return sb.toString();
     }
     private String coordinatesToLocation(int x, int y) { // 좌표를 체스판 맵의 키로 변환
-        String location = indexToLocation(x * COL_SIZE + y);
+        String location = indexToLocation(x * colSize + y);
         return location;
     }
-    public void addEmpty(Pawn pawn) { // 체스판에 폰을 추가
+    public String addEmpty(Pawn pawn) { // 체스판에 빈 공간 중 맨 앞에 폰을 추가
+        String location;
         try {
+            location = findEmpty();
             pawnMap.put(findEmpty(), pawn);
+            return location;
         } catch(Exception e) {
-            System.out.println(e.getMessage());
+            return null;
         }
     }
     public void add(Pawn pawn, String location) throws Exception {
-        if (location.matches("[A-H][1-8]")) {
+        StringBuilder sb = new StringBuilder();
+        String regex;
+        sb.append("[A-][1-]");
+        sb.insert(3, (char)('A' + colSize));
+        sb.insert(8, rowSize);
+        regex = sb.toString();
+        if (location.matches(regex)) {
             pawnMap.put(location, pawn);
         } else {
             throw new Exception("Location 형식이 틀림");
@@ -76,6 +97,9 @@ public class Board {
     public int size() {
         return pawnMap.size();
     }
+    public Pawn findPawn(String location) {
+        return pawnMap.get(location);
+    }
     public Pawn findPawn(int index) { // 체스판의 인덱스 위치에 있는 폰을 찾음
         String location = indexToLocation(index);
         return pawnMap.get(location);
@@ -87,8 +111,8 @@ public class Board {
     public String printBoard() {
         StringBuilder sb = new StringBuilder();
         String location;
-        for (int i = 0; i < ROW_SIZE; i++) {
-            for (int j = 0; j < COL_SIZE; j++) {
+        for (int i = 0; i < rowSize; i++) {
+            for (int j = 0; j < colSize; j++) {
                 location = coordinatesToLocation(j, i);
                 if (pawnMap.containsKey(location)) {
                     sb.append(pawnMap.get(location).getRepresentation());
