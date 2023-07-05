f90f841a3187f8dcb8e3c1959e8b6621c937536f
socra167 <socra167@gmail.com>
2023-07-05 10:21:04 +0900

    refactor: 체스판의 자료구조를 List에서 Map으로 변경, 좌표 및 인덱스로 폰을 추가할 수 있는 인터페이스 구현

--- src/main/java/softeer2nd/chess/Board.java
+++ src/main/java/softeer2nd/chess/Board.java
@@ -2,7 +2,6 @@ package softeer2nd.chess;
 
 import softeer2nd.chess.pieces.Pawn;
 
-import java.awt.List;
 import java.util.*;
 
 public class Board {
@@ -10,7 +9,26 @@ public class Board {
     private static final int COL_SIZE = 8;
     Map<String, Pawn> pawnMap = new HashMap<>();
 
-    public String findEmpty() throws Exception { // 체스판의 비어 있는 공간 중 맨 앞 찾기
+    Board() {
+
+    }
+    public void initialize() throws Exception {
+        Pawn pawn;
+        for (int i = 0; i < ROW_SIZE; i++) {
+            if (i == 1) {
+                for (int j = 0; j < COL_SIZE; j++) {
+                    pawn = new Pawn(Color.BLACK);
+                    add(pawn, j, i);
+                }
+            } else if (ROW_SIZE - i == 1) {
+                for (int j = 0; j < COL_SIZE; i++) {
+                    pawn = new Pawn(Color.WHITE);
+                    add(pawn, j, i);
+                }
+            }
+        }
+    }
+    public String findEmpty() throws Exception { // 체스판의 비어 있는 공간 중 맨 앞의 키 반환
         Set<String> keys = pawnMap.keySet();
         StringBuilder sb = new StringBuilder();
         String location;
@@ -24,29 +42,46 @@ public class Board {
                 sb.setLength(0);
             }
         }
-        throw new Exception("비어 있는 공간이 없습니다");
+        throw new Exception("비어 있는 공간이 없음");
     }
-
-    public String indexToLocation(int index) { // index 값을 체스판 맵의 키로 변환
+    private String indexToLocation(int index) { // index 값을 체스판 맵의 키로 변환
         StringBuilder sb = new StringBuilder();
         String location;
         sb.append((char)('A' + (index / COL_SIZE)));
         sb.append(index % COL_SIZE + 1);
         return sb.toString();
     }
-
-    public void add(Pawn pawn) { // 체스판에 폰을 추가
+    private String coordinatesToLocation(int x, int y) { // 좌표를 체스판 맵의 키로 변환
+        String location = indexToLocation(x * COL_SIZE + y);
+        return location;
+    }
+    public void addEmpty(Pawn pawn) { // 체스판에 폰을 추가
         try {
             pawnMap.put(findEmpty(), pawn);
         } catch(Exception e) {
             System.out.println(e.getMessage());
         }
     }
+    public void add(Pawn pawn, String location) throws Exception {
+        if (location.matches("[A-H][1-8]")) {
+            pawnMap.put(location, pawn);
+        } else {
+            throw new Exception("Location 형식이 틀림");
+        }
+    }
+    public void add(Pawn pawn, int x, int y) throws Exception {
+        String location = coordinatesToLocation(x, y);
+        add(pawn, location);
+    }
     public int size() {
         return pawnMap.size();
     }
     public Pawn findPawn(int index) { // 체스판의 인덱스 위치에 있는 폰을 찾음
-        System.out.println(indexToLocation(index));
-        return pawnMap.get(indexToLocation(index));
+        String location = indexToLocation(index);
+        return pawnMap.get(location);
+    }
+    public Pawn findPawn(int x, int y) { // 체스판의 특정 좌표에 있는 폰을 찾음
+        String location = coordinatesToLocation(x, y);
+        return pawnMap.get(location);
     }
 }
