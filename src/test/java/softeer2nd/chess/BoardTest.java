ff7b3bf60485df6a892015462971e2f45ba6572d
socra167 <socra167@gmail.com>
2023-07-07 13:42:01 +0900

    test: 기물과 색에 해당하는 기물의 수를 반환하는 테스트 작성, 테스트 계층화

--- src/test/java/softeer2nd/chess/BoardTest.java
+++ src/test/java/softeer2nd/chess/BoardTest.java
@@ -2,8 +2,10 @@ package softeer2nd.chess;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import softeer2nd.chess.pieces.Piece;
+import softeer2nd.chess.pieces.Piece.Type;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static softeer2nd.chess.utils.StringUtils.appendNewLine;
@@ -44,6 +46,41 @@ public class BoardTest {
         assertEquals(piece, board.findPiece(location));
     }
 
+    @Nested
+    @DisplayName("체스판을 초기화 하면")
+    public class AfterInitialize {
+        @Test
+        @DisplayName("흰색 Pawn 열과 검은색 Pawn 열의 결과가 정상이어야 한다")
+        public void initialize() {
+            board = new Board();
+            assertEquals("pppppppp", board.getWhitePieceResult());
+            assertEquals("PPPPPPPP", board.getBlackPieceResult());
+        }
+
+        @Test
+        @DisplayName("Blank가 아닌 기물이 32개 생성된다")
+        public void pieceCount() {
+            board = new Board();
+            assertEquals(board.pieceCount(), 32);
+        }
+
+        @Test
+        @DisplayName("전체 상태를 출력하면 말이 순서에 맞게 놓인 상태로 출력된다")
+        public void create() {
+            board.initialize();
+            System.out.println(board.showBoard());
+            assertEquals(32, board.pieceCount());
+            String blankRank = appendNewLine("........");
+            assertEquals(
+                    appendNewLine("RNBQKBNR") +
+                            appendNewLine("PPPPPPPP") +
+                            blankRank + blankRank + blankRank + blankRank +
+                            appendNewLine("pppppppp") +
+                            appendNewLine("rnbqkbnr"),
+                    board.showBoard());
+        }
+    }
+
     @Test
     @DisplayName("체스판의 빈 공간에 폰을 추가하면 체스판의 기물 수가 커지고, 추가하고자 한 폰은 체스판에 추가된 폰과 일치해야 한다")
     public void createPawn() {
@@ -59,33 +96,11 @@ public class BoardTest {
     }
 
     @Test
-    @DisplayName("체스판 초기화 시 흰색 Pawn 열과 검은색 Pawn 열의 결과가 정상이어야 한다")
-    public void initialize() {
-        board = new Board();
-        assertEquals("pppppppp", board.getWhitePieceResult());
-        assertEquals("PPPPPPPP", board.getBlackPieceResult());
-    }
-
-    @Test
-    @DisplayName("체스판 초기화 시 말이 32개 생성된다")
-    public void pieceCount() {
-        board = new Board();
-        assertEquals(board.pieceCount(), 32);
-    }
-
-    @Test
-    @DisplayName("체스판 초기화 후 전체 상태를 출력하면 말이 순서에 맞게 놓인 상태로 출력된다")
-    public void create() {
-        board.initialize();
-        System.out.println(board.showBoard());
-        assertEquals(32, board.pieceCount());
-        String blankRank = appendNewLine("........");
-        assertEquals(
-                appendNewLine("RNBQKBNR") +
-                        appendNewLine("PPPPPPPP") +
-                        blankRank + blankRank + blankRank + blankRank +
-                        appendNewLine("pppppppp") +
-                        appendNewLine("rnbqkbnr"),
-                board.showBoard());
+    @DisplayName("기물과 색에 해당하는 기물의 수를 반환할 수 있다")
+    public void getPieceCount() {
+        assertEquals(8, board.getPieceCount(Piece.Color.WHITE, Type.PAWN));
+        assertEquals(2, board.getPieceCount(Piece.Color.WHITE, Type.ROOK));
+        assertEquals(1, board.getPieceCount(Piece.Color.WHITE, Type.KING));
+        assertEquals(1, board.getPieceCount(Piece.Color.BLACK, Type.KING));
     }
 }
