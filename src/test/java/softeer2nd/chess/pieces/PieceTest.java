6aa5e399dc81c80fc962f3d21afdadb333918961
socra167 <socra167@gmail.com>
2023-07-07 09:58:12 +0900

    test: 같은 종류이고 색이 다른 Piece 쌍을 생성했을 때 종류가 일치하는지 확인하는 테스트, blank piece의 색상이 white 또는 black이 아님을 테스트

--- src/test/java/softeer2nd/chess/pieces/PieceTest.java
+++ src/test/java/softeer2nd/chess/pieces/PieceTest.java
@@ -1,11 +1,21 @@
 package softeer2nd.chess.pieces;
 
+import softeer2nd.chess.pieces.Piece.Type;
 import org.junit.jupiter.api.*;
+
+import java.awt.*;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PieceTest {
+
+    @Test
+    @DisplayName("색에 알맞은 폰이 생성되어야 한다")
+    public void create() {
+        verifyPiece(Piece.Color.WHITE);
+        verifyPiece(Piece.Color.BLACK);
+    }
 
     private void verifyPiece(Piece.Color color) {
         Piece piece;
@@ -16,39 +26,32 @@ public class PieceTest {
         }
         assertThat(piece.getColor()).isEqualTo(color);
     }
+
+    @Test
+    @DisplayName("생성된 Piece의 Color와 Representation이 일치한다")
+    public void create_piece_check() {
+        verifyPiece(Piece.createWhitePawn(), Piece.Color.WHITE, Type.PAWN.getWhiteRepresentation());
+        verifyPiece(Piece.createBlackPawn(), Piece.Color.BLACK, Type.PAWN.getBlackRepresentation());
+
+        verifyPiece(Piece.createWhiteKnight(), Piece.Color.WHITE, Type.KNIGHT.getWhiteRepresentation());
+        verifyPiece(Piece.createBlackKnight(), Piece.Color.BLACK, Type.KNIGHT.getBlackRepresentation());
+
+        verifyPiece(Piece.createWhiteRook(), Piece.Color.WHITE, Type.ROOK.getWhiteRepresentation());
+        verifyPiece(Piece.createBlackRook(), Piece.Color.BLACK, Type.ROOK.getBlackRepresentation());
+
+        verifyPiece(Piece.createWhiteBishop(), Piece.Color.WHITE, Type.BISHOP.getWhiteRepresentation());
+        verifyPiece(Piece.createBlackBishop(), Piece.Color.BLACK, Type.BISHOP.getBlackRepresentation());
+
+        verifyPiece(Piece.createWhiteQueen(), Piece.Color.WHITE, Type.QUEEN.getWhiteRepresentation());
+        verifyPiece(Piece.createBlackQueen(), Piece.Color.BLACK, Type.QUEEN.getBlackRepresentation());
+
+        verifyPiece(Piece.createWhiteKing(), Piece.Color.WHITE, Type.KING.getWhiteRepresentation());
+        verifyPiece(Piece.createBlackKing(), Piece.Color.BLACK, Type.KING.getBlackRepresentation());
+    }
+
     private void verifyPiece(final Piece piece, final Piece.Color color, final char representation) {
         assertEquals(color, piece.getColor());
         assertEquals(representation, piece.getRepresentation());
-    }
-
-
-    @Test
-    @DisplayName("색에 알맞은 폰이 생성되어야 한다")
-    public void create() {
-        verifyPiece(Piece.Color.WHITE);
-        verifyPiece(Piece.Color.BLACK);
-    }
-
-    @Test
-    @DisplayName("각 Piece별로 흰색 말과 검은 색 말을 생성할 수 있다")
-    public void create_piece() {
-        verifyPiece(Piece.createWhitePawn(), Piece.Color.WHITE, Piece.Type.PAWN.getWhiteRepresentation());
-        verifyPiece(Piece.createBlackPawn(), Piece.Color.BLACK, Piece.Type.PAWN.getBlackRepresentation());
-
-        verifyPiece(Piece.createWhiteKnight(), Piece.Color.WHITE, Piece.Type.KNIGHT.getWhiteRepresentation());
-        verifyPiece(Piece.createBlackKnight(), Piece.Color.BLACK, Piece.Type.KNIGHT.getBlackRepresentation());
-
-        verifyPiece(Piece.createWhiteRook(), Piece.Color.WHITE, Piece.Type.ROOK.getWhiteRepresentation());
-        verifyPiece(Piece.createBlackRook(), Piece.Color.BLACK, Piece.Type.ROOK.getBlackRepresentation());
-
-        verifyPiece(Piece.createWhiteBishop(), Piece.Color.WHITE, Piece.Type.BISHOP.getWhiteRepresentation());
-        verifyPiece(Piece.createBlackBishop(), Piece.Color.BLACK, Piece.Type.BISHOP.getBlackRepresentation());
-
-        verifyPiece(Piece.createWhiteQueen(), Piece.Color.WHITE, Piece.Type.QUEEN.getWhiteRepresentation());
-        verifyPiece(Piece.createBlackQueen(), Piece.Color.BLACK, Piece.Type.QUEEN.getBlackRepresentation());
-
-        verifyPiece(Piece.createWhiteKing(), Piece.Color.WHITE, Piece.Type.KING.getWhiteRepresentation());
-        verifyPiece(Piece.createBlackKing(), Piece.Color.BLACK, Piece.Type.KING.getBlackRepresentation());
     }
 
     @Test
@@ -60,5 +63,33 @@ public class PieceTest {
         assertThat(Piece.isBlack(whitePawn)).isEqualTo(false);
         assertThat(Piece.isWhite(blackPawn)).isEqualTo(false);
         assertThat(Piece.isBlack(blackPawn)).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("같은 종류이고 색이 다른 Piece 쌍을 생성하면 종류가 일치해야 한다")
+    public void create_piece() {
+        verifyPiece(Piece.createWhitePawn(), Piece.createBlackPawn(), Type.PAWN);
+        verifyPiece(Piece.createWhiteKnight(), Piece.createBlackKnight(), Type.KNIGHT);
+        verifyPiece(Piece.createWhiteRook(), Piece.createBlackRook(), Type.ROOK);
+        verifyPiece(Piece.createWhiteBishop(), Piece.createBlackBishop(), Type.BISHOP);
+        verifyPiece(Piece.createWhiteQueen(), Piece.createBlackQueen(), Type.QUEEN);
+        verifyPiece(Piece.createWhiteKing(), Piece.createBlackKing(), Type.KING);
+    }
+
+    @Test
+    @DisplayName("기물이 존재하지 않는 Piece를 생성하면 White 또는 Black이 아니어야 한다")
+    public void create_blankPiece() {
+        Piece blank = Piece.createBlank();
+        assertFalse(blank.isWhite());
+        assertFalse(blank.isBlack());
+        assertEquals(Type.NO_PIECE, blank.getType);
+    }
+
+    private void verifyPiece(final Piece whitePiece, final Piece blackPiece, final Type type) {
+        assertTrue(whitePiece.isWhite());
+        assertEquals(type, whitePiece.getType);
+
+        assertTrue(blackPiece.isBlack());
+        assertEquals(type, blackPiece.getType());
     }
 }
